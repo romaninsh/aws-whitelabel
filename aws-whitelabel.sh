60cd272a00c4d28b4cf17bc653b1c53409ebc4d5
Romans Malinovskis <romans@agiletoolkit.org>
2018-07-31 01:09:17 +0300

    handle exit status and dry run properly

--- aws-whitelabel.sh
+++ aws-whitelabel.sh
@@ -7,13 +7,17 @@ while true; do
 
     $W
 
+
     if [ $? -eq 2 ]; then
+
+        [ "$DRY_RUN" ] && exit
+
         echo
         echo
         echo "Waiting 10 minutes, then will retry"
         sleep 300
 
     else
-        break
+        exit $?
     fi
 done
